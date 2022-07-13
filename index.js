c9ff0ea30f2f26cfde3c681b7c3d5dd50e523121
PanosKontos <panos96el@gmail.com>
2022-07-13 23:45:55 +0300

    first commit

--- index.js
+++ index.js
@@ -17,3 +17,17 @@ console.log(variables.name)
 
 // importing function
 variables.printName()
+
+
+// Run our server
+const express = require('express')
+const app = express()
+
+// req = request
+// res = response
+app.get("/",(req,res)=>{ res.send('<h1> Hi Mark </h1>') })
+app.get("/about",(req,res)=>{ res.send('<h1> About page </h1>') })
+
+
+// port
+app.listen(3000,()=>{console.log("Server is running on port 3000")})
