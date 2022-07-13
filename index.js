20cafbab0edc3a3a1b4484bfad16fecedac9abf5
PanosKontos <panos96el@gmail.com>
2022-07-13 23:32:09 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log(2+4)
+
+// whenever you import library 
+// use require
+var _ = require('lodash')
+
+var a = _.chunk(['a', 'b', 'c', 'd'], 2);
+
+console.log(a)
+
+
+
+// Importing files
+var variables = require('./vars')
+console.log(variables)
+console.log(variables.name)
---
+++ vars.js
@@ -0,0 +1,10 @@
+var name = "Panos"
+var age = 29
+var state = "ATH"
+
+// exporting data
+module.exports = {
+    name:name, 
+    age:age,
+    state:state
+}
